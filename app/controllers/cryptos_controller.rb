0e1c0f5a061d802aa9469ba469207838736ebc7c
fabricio zanette <fabzanette@gmail.com>
2018-09-12 18:54:03 -0300

    config controller to fetch data from api

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,7 +1,21 @@
 class CryptosController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  require 'json'
+  require 'open-uri'
 
   def index
-
+    @coins = []
+    @cryptos = fetch_data_from_api
+    @cryptos.each do |crypto|
+      @coins << {'nome' => crypto['name'],
+                 'preco' => crypto['price_usd']}
+    end
+    @coins
   end
 
+  def fetch_data_from_api
+    url = "https://api.coinmarketcap.com/v1/ticker/"
+    uri = open(url).read
+    parsed_data = JSON.parse(uri)
+  end
 end
