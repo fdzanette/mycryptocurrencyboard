7bd47dc6c9ee073604293d4a4b9022c87b9d22f7
fabricio zanette <fabzanette@gmail.com>
2018-09-14 15:38:50 -0300

    get the elements needed to autocomplete form. not working fully yet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,16 @@
 import "bootstrap";
 
-const add_button = document.getElementById('btn-add');
+const currencys = document.querySelectorAll('.add-to-portfolio');
+const add_button = document.querySelectorAll('.crypto-name');
+const addNameToField = document.getElementById('crypto_name');
 
-add_button.addEventListener('click', (event) => {
+let i = 0;
+currencys.forEach((currency) => {
+  console.log(add_button[i].innerHTML);
+  i +=1;
+});
+
+currencys.addEventListener('click', (event) => {
   console.log(add_button.innerHTML);
 });
 
