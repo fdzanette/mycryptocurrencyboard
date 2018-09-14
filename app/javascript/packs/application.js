6ae85d0d3b45d445957795ce192408169423e7b3
fabricio zanette <fabzanette@gmail.com>
2018-09-14 16:51:10 -0300

    javascript function correct but not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,20 @@
 import "bootstrap";
 
-const currencys = document.querySelectorAll('.add-to-portfolio');
 const add_button = document.querySelectorAll('.crypto-name');
 const addNameToField = document.getElementById('crypto_name');
 
+let coinName;
 let i = 0;
-currencys.forEach((currency) => {
-  console.log(add_button[i].innerHTML);
-  i +=1;
-});
 
-currencys.addEventListener('click', (event) => {
-  console.log(add_button.innerHTML);
-});
+for (let i = 0; i < add_button.length; i++) {
+  coinName = document.getElementById(add_button[i].innerHTML)
+  coinName.addEventListener('click', (event) => {
+   console.log(add_button[i].innerHTML)
+   if (coinName.id === add_button[i].innerHTML) {
+      document.getElementById('crypto_name').value = coinName.id;
+   } else {}
+  });
+};
+i +=1;
+
 
